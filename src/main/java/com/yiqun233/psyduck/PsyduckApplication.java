420f5f9a5e0ee038df1cf812b44e6a81708438de
Qun Q Yi <1234@qwer>
2023-03-06 15:36:40 +0800

    Initial commit

---
+++ src/main/java/com/yiqun233/psyduck/PsyduckApplication.java
@@ -0,0 +1,16 @@
+package com.yiqun233.psyduck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Qun Q Yi
+ */
+@SpringBootApplication
+public class PsyduckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PsyduckApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yiqun233/psyduck/PsyduckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yiqun233.psyduck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsyduckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
