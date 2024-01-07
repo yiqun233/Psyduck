af9ab282daa7192a8c811f04c5961a290d951935
yiqun <qun.q.yi@cn.pwc.com>
2024-01-07 18:18:47 +0800

    石油与天然气地质

---
+++ src/main/java/com/yiqun233/psyduck/reptile/util/SimpleWebScraper.java
@@ -0,0 +1,29 @@
+package com.yiqun233.psyduck.reptile.util;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class SimpleWebScraper {
+    public static void main(String[] args) {
+        // 设置WebDriver的路径
+        System.setProperty("webdriver.edge.driver", "C:\\Windows\\Software\\edgedriver_win64\\msedgedriver.exe");
+
+        // 创建Chrome浏览器实例
+        WebDriver driver = new EdgeDriver();
+
+        // 打开目标网页
+        driver.get("http://www.cpedm.com/CN/10.11698/PED.20230152#9");
+
+        // 执行JavaScript代码，以模拟浏览器行为（如果需要）
+        // driver.executeScript("javascript:...");
+
+        // 获取HTML内容
+        String htmlContent = driver.getPageSource();
+
+        // 打印HTML内容
+        System.out.println(htmlContent);
+
+// 关闭浏览器
+        driver.quit();
+    }
+}
